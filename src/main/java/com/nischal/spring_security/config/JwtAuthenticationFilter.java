ba88974cb24d40f8a04b6f2c5d79e458f55f6b3a
Nischal876 <nischalaryal11@gmail.com>
2024-07-23 17:37:12 +0545

    Added logic to check header validity by checking if it is null or does not start with 'Bearer' - JWT is extracted by removing 'Bearer' prefix if the header is valid - Added methods to extract all claims and get Sing in key in JwtService class

--- src/main/java/com/nischal/spring_security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/nischal/spring_security/config/JwtAuthenticationFilter.java
@@ -1,12 +1,12 @@
 package com.nischal.spring_security.config;
 
+import com.nischal.spring_security.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,13 +21,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // operations
     //FilterChain is the chain of responsibilities
 
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
-    )
-            throws ServletException, IOException {
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
---
+++ src/main/java/com/nischal/spring_security/service/JwtService.java
@@ -0,0 +1,31 @@
+package com.nischal.spring_security.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "ab54f21942c9b9f3c6c6da1d812dc2d40957e5ac896c100027f5495e663f60aa";
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
