f0a5216f42e7e711930a9e3b83b450713796bab3
Nischal876 <nischalaryal11@gmail.com>
2024-07-23 11:51:26 +0545

    - Created UserRepository forUser Entity - Extend JpaRepository to provide CRUD operations for USer entity - Added method to find user by email

---
+++ src/main/java/com/nischal/spring_security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nischal.spring_security.repository;
+
+import com.nischal.spring_security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
