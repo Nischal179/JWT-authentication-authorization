405a0661dcc231b8590f66692695849dc958331f
Nischal876 <nischalaryal11@gmail.com>
2024-07-23 13:34:37 +0545

    Add JwtAuthenticationFilter class to intercept and handle HTTP requests

---
+++ src/main/java/com/nischal/spring_security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.nischal.spring_security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    // We can intercept HTTP requests and perform operations and return
+    // response based on the output of performed
+    // operations
+    //FilterChain is the chain of responsibilities
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    )
+            throws ServletException, IOException {
+
+    }
+}
